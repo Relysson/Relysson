b899b288bfa02738e561ae28e2e3a41b44ffd55e
relys <relys@DESKTOP-B6V853H.local>
2022-04-19 21:29:40 -0300

    alteração e criação

--- primeiro-programa-java/src/executavel/PrimeiraClasse.java
+++ primeiro-programa-java/src/executavel/PrimeiraClasse.java
@@ -3,7 +3,8 @@ package executavel;
 import java.security.DomainCombiner;
 
 public class PrimeiraClasse {
-
+	/* teste commit*/
+	
 	public static void main(String[] args) {
 		System.out.println("meu primeiro programa em java");
 		System.out.println("java testando 123");
