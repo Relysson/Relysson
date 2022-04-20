b8aec828d317b1ca24465c66210da7d1122fbed3
relys <relys@DESKTOP-B6V853H.local>
2022-04-19 21:06:18 -0300

    criação do projeto

---
+++ primeiro-programa-java/src/executavel/PrimeiraClasse.java
@@ -0,0 +1,13 @@
+package executavel;
+
+import java.security.DomainCombiner;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		System.out.println("meu primeiro programa em java");
+		System.out.println("java testando 123");
+
+	}
+
+}
